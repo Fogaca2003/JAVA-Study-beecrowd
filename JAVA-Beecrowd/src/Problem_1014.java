cadd5095f3c3838a5c74733240f77afb905b9412
Fogaca2003 <sebastiao7.fogaca@gmail.com>
2026-05-13 16:55:52 -0400

    add Problem 1014

--- JAVA-Beecrowd/src/Problem_1014.java
+++ JAVA-Beecrowd/src/Problem_1014.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Problem_1014 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int total_distance;
+        double fuel_consumed, kmL;
+
+        total_distance = sc.nextInt();
+        fuel_consumed = sc.nextDouble();
+
+        kmL = (double) total_distance / fuel_consumed;
+
+        System.out.printf("%.3f km/l%n", kmL);
+
+        sc.close();
+    }
 }
